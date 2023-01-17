559bd638b3eb80ae80390032e9b594112e756b67
Irfan <shaikh.irfan.oct16@gmail.com>
2023-01-17 21:37:35 +0530

    Made layout for SubFrame

--- src/SubFrame.java
+++ src/SubFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,7 +25,9 @@ public class SubFrame extends JFrame {
     JButton playButton;
     JButton backButton;
 
+    JPanel panel = new JPanel();
     JPanel leaderbroadPanel = new JPanel();
+    JLabel leaderbroadLabel;
 
     SubFrame(String username, int game) {
 
@@ -36,7 +40,7 @@ public class SubFrame extends JFrame {
 
         // main frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 800);
+        frame.setSize(600, 829);
         frame.getContentPane().setBackground(new Color(50, 50, 50));
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
@@ -44,7 +48,7 @@ public class SubFrame extends JFrame {
         frame.setTitle("User : " + username);
 
         // Title panel
-        titlePanel.setPreferredSize(new Dimension(550, 100));
+        titlePanel.setPreferredSize(new Dimension(550, 120));
         titlePanel.setLayout(new BorderLayout());
         titlePanel.setBackground(new Color(8, 36, 80));
 
@@ -53,19 +57,22 @@ public class SubFrame extends JFrame {
         gameTitle.setForeground(new Color(255, 214, 0));
         titlePanel.add(gameTitle, BorderLayout.NORTH);
 
-        highscoreLabel = new JLabel(Integer.toString(highScore), JLabel.CENTER);
-        highscoreLabel.setFont(new Font(fontName, Font.BOLD, 50));
+        highscoreLabel = new JLabel("Highscore : " + highScore, JLabel.CENTER);
+        highscoreLabel.setFont(new Font(fontName, Font.BOLD, 34));
         highscoreLabel.setForeground(new Color(255, 214, 0));
         titlePanel.add(highscoreLabel, BorderLayout.SOUTH);
 
         // Button panel
-        buttonPanel.setLayout(new BorderLayout());
+        buttonPanel.setLayout(new FlowLayout());
         buttonPanel.setPreferredSize(new Dimension(600, 100));
         buttonPanel.setBackground(new Color(8, 36, 80));
         playButton = new JButton();
         playButton.setText("PLAY");
-        playButton.setFont(new Font(fontName, Font.BOLD, 50));
+        playButton.setFont(new Font(fontName, Font.BOLD, 35));
         playButton.setForeground(new Color(255, 214, 0));
+        playButton.setPreferredSize(new Dimension(170, 50));
+        playButton.setBorder(new LineBorder(Color.white, 10, false));
+
         playButton.addActionListener(new ActionListener() {
 
             @Override
@@ -86,8 +93,10 @@ public class SubFrame extends JFrame {
         });
         backButton = new JButton();
         backButton.setText("BACK");
-        backButton.setFont(new Font(fontName, Font.BOLD, 50));
+        backButton.setFont(new Font(fontName, Font.BOLD, 35));
         backButton.setForeground(new Color(255, 214, 0));
+        backButton.setPreferredSize(new Dimension(170, 50));
+        backButton.setBorder(new LineBorder(Color.white, 10, false));
         backButton.addActionListener(new ActionListener() {
 
             @Override
@@ -96,28 +105,38 @@ public class SubFrame extends JFrame {
                 frame.dispose();
             }
         });
-        buttonPanel.add(playButton, BorderLayout.EAST);
-        buttonPanel.add(backButton, BorderLayout.WEST);
+        buttonPanel.add(playButton);
+        buttonPanel.add(backButton);
 
         // Leaderbroad panel
-        leaderbroadPanel.setPreferredSize(new Dimension(550, 600));
+        panel.setLayout(new BorderLayout());
+        panel.setBorder(new LineBorder(Color.red, 10, false));
+
+        leaderbroadPanel.setPreferredSize(new Dimension(550, 550));
         leaderbroadPanel.setLayout(new GridLayout(10, 3));
         leaderbroadPanel.setBackground(new Color(8, 36, 80));
+
+        leaderbroadLabel = new JLabel("Leaderbroad", JLabel.CENTER);
+        leaderbroadLabel.setFont(new Font(fontName, Font.BOLD, 30));
+        panel.add(leaderbroadLabel, BorderLayout.NORTH);
 
         int i = 0;
         for (User user : top10Users) {
             i++;
-            // System.out.println("Username: " + user.username + " Score: " + user.score);
-
             JLabel ranklabel = new JLabel(Integer.toString(i), JLabel.CENTER);
             ranklabel.setFont(new Font(fontName, Font.BOLD, 25));
             ranklabel.setForeground(new Color(255, 214, 0));
+            ranklabel.setPreferredSize(new Dimension(550, 30));
+
             JLabel usernameLabel = new JLabel(user.username, JLabel.CENTER);
             usernameLabel.setFont(new Font(fontName, Font.BOLD, 25));
             usernameLabel.setForeground(new Color(255, 214, 0));
+            usernameLabel.setPreferredSize(new Dimension(550, 30));
+
             JLabel scoreLable = new JLabel(Integer.toString(user.score), JLabel.CENTER);
             scoreLable.setFont(new Font(fontName, Font.BOLD, 25));
             scoreLable.setForeground(new Color(255, 214, 0));
+            scoreLable.setPreferredSize(new Dimension(550, 30));
 
             JPanel userPanel = new JPanel();
             userPanel.setLayout(new GridLayout(1, 3));
@@ -125,17 +144,19 @@ public class SubFrame extends JFrame {
             userPanel.add(usernameLabel);
             userPanel.add(scoreLable);
 
+            userPanel.setBorder(new LineBorder(Color.red, 10, false));
             leaderbroadPanel.add(userPanel);
         }
 
+        panel.add(leaderbroadPanel, BorderLayout.SOUTH);
         frame.add(titlePanel, BorderLayout.NORTH);
         frame.add(buttonPanel, BorderLayout.CENTER);
-        frame.add(leaderbroadPanel, BorderLayout.SOUTH);
+        frame.add(panel, BorderLayout.SOUTH);
 
     }
 
     public static void main(String[] args) {
-        new SubFrame("irfan1", 3);
+        new SubFrame("irfan", 3);
     }
 
 }
